6f7a54a01b24ba2dce394fdced3875b6e108464a
Jankarlo Villanueva <jankarlo.villanueva1@marist.edu>
2023-03-28 20:18:53 -0400

    modified:   project/main.py 	new file:   project/movies.py 	new file:   project/scratch/user.py 	new file:   project/scratch/users.txt 	new file:   project/test_main.py

--- project/main.py
+++ project/main.py
@@ -1,5 +1,9 @@
 # import the flask package for making rest apis.
 from flask import Flask
+from flask import *
+from movies import movies 
+
+
 
 # create an instance of a Flask object.
 app = Flask(__name__)
@@ -7,11 +11,29 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello_world():
+
     '''
     This is our base route. When visited, we get the text "Hello, world!"
     '''
     return "Hello, world!"
 
+@app.route("/api/movies", methods=["GET"])
+def list_movies():
+    """
+    This function returns a Response object back containing all
+    of stored movies and reviews back to the caller on the web.
+    """
+    response_body = movies
+
+    titles = request.args.getlist("title")
+
+    # only get the movies that have the same title as the query parameter.
+    if titles != []:
+        response_body = [movie for movie in movies if movie["title"] in titles]
+
+    return jsonify(response_body)
+
 
 # run our application
-app.run()
+if __name__ == '__main__':
+    app.run()
---
+++ project/scratch/user.py
@@ -0,0 +1,13 @@
+#Define a user object 
+
+class user():
+
+    def __init__(self, first_name, last_name, userid, email, role):
+        self.first_name= first_name
+        self.last_name= last_name
+        self.userid = userid
+        self.email= email
+        self.role= user
+
+
+open
